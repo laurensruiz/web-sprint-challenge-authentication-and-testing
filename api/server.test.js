7f10b72becf8b1e9d3ac1929b4ebfdfc03c25b9f
laurensruiz <laurenruiz97@yahoo.com>
2022-05-27 03:59:19 -0400

    fixed error

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,17 @@
+const request = require('supertest')
+const db = require('../data/dbConfig')
+const server= require('./server')
 // Write your tests here
 test('sanity', () => {
   expect(true).toBe(true)
 })
+
+beforeAll(async ()=>{
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+beforeEach(async ()=>{
+  await db('jokes').truncate()
+})
+
